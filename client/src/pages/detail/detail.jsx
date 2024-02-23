17384472a544dc98f07e3c42008e8eb9474914f5
luisdefago <defagoluis5@gmail.com>
2024-02-23 02:39:42 -0300

    render detail driverDB

--- client/src/pages/detail/detail.jsx
+++ client/src/pages/detail/detail.jsx
@@ -5,67 +5,83 @@ import "./detail.css";
 
 const Detail = () => {
   const { id } = useParams();
-  const [driver, setDriver] = useState([]);
+  const [driver, setDriver] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
       .get(`http://127.0.0.1:3001/drivers/driverById/${id}`)
       .then(({ data }) => {
         if (!data || data.length === 0) {
-          alert("No hay conductor con ese ID");
-          setDriver([]);
+          setError("No se encontró ningún conductor con ese ID");
         } else {
-          setDriver(data);
+          if (Array.isArray(data[0])) {
+            setDriver(data[0][0]);
+          } else {
+            setDriver(data);
+          }
         }
       })
       .catch((error) => {
         console.error("Error al obtener detalles del conductor:", error);
-        alert("Hubo un error al obtener detalles del conductor.");
-      });
+        setError("Hubo un error al obtener detalles del conductor");
+      })
+      .finally(() => setLoading(false));
   }, [id]);
 
-  return (
-    <main className="detailPage">
-      {driver.length === 0 ? (
+  if (loading) {
+    return (
+      <main className="detailPage">
         <img
           className="detailCarga"
           src="/assets/auto-f1.png"
-          alt="Loading..."
+          alt="Cargando..."
         />
-      ) : (
-        <section className="detail">
-          <div className="detailInfImg">
-            <div className="detailConteinInf">
-              <div className="detailContName">
-                <h3 className="detailName">
-                  Nombre: {driver[0][0]?.name?.forename}
-                </h3>
-                <h3 className="detailName">
-                  Apellido: {driver[0][0]?.name?.surname}
-                </h3>
-              </div>
-              <p className="detailInf">Teams: {driver[0][0]?.teams}</p>
-              <p className="detailInf">
-                Nationality: {driver[0][0]?.nationality}
-              </p>
-              <p className="detailInf">Dob: {driver[0][0]?.dob}</p>
-              <p className="detailInf">Id: {driver[0][0]?.id}</p>
+      </main>
+    );
+  }
+
+  if (error) {
+    return (
+      <main className="detailPage">
+        <p>Error: {error}</p>
+      </main>
+    );
+  }
+
+  console.log(driver);
+
+  return (
+    <main className="detailPage">
+      <section className="detail">
+        <div className="detailInfImg">
+          <div className="detailConteinInf">
+            <div className="detailContName">
+              <h3 className="detailName">
+                Nombre: {driver?.name?.forename || driver?.forename}
+              </h3>
+              <h3 className="detailName">
+                Apellido: {driver?.name?.surname || driver?.surname}
+              </h3>
             </div>
-            <div className="det-cont-img">
-              <img
-                className="detailImg detail-img"
-                src={driver[0][0]?.image?.url}
-                alt={`${driver[0][0]?.name?.forename} ${driver[0][0]?.name?.surname}`}
-              />
-            </div>
+            <p className="detailInf">Teams: {driver?.teams}</p>
+            <p className="detailInf">Nationality: {driver?.nationality}</p>
+            <p className="detailInf">Dob: {driver?.dob}</p>
+            <p className="detailInf">Id: {driver?.id}</p>
           </div>
-          {driver[0][0]?.description ? (
-            <p className="detailDescrip">
-              Description: {driver[0][0]?.description}
-            </p>
-          ) : null}
-        </section>
-      )}
+          <div className="det-cont-img">
+            <img
+              className="detailImg detail-img"
+              src={driver?.image?.url || driver?.image}
+              alt={`${driver?.name?.forename} ${driver?.name?.surname}`}
+            />
+          </div>
+        </div>
+        {driver?.description && (
+          <p className="detailDescrip">Description: {driver?.description}</p>
+        )}
+      </section>
     </main>
   );
 };
